2862f2acc4600a6a04fff54a718048550e09e86a
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-09-20 23:22:05 +0530

    add react project

---
+++ apps/react-project/src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+import { Button } from '@repo/ui/button'
+
+function App() {
+  return (
+    <>
+      <Button appName='React Project'>Hi from React Project!</Button>
+    </>
+  )
+}
+
+export default App
