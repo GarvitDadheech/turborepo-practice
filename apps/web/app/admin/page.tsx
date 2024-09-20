8ae19f55a5c5b08033972602d2988376536a54c5
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-09-20 23:13:25 +0530

    add simple admin page

---
+++ apps/web/app/admin/page.tsx
@@ -0,0 +1,13 @@
+"use client"
+import { Button } from "@repo/ui/button"
+
+const Admin = () => {
+    return (
+        <div>
+            <h1>This is the Admin Page!</h1>
+            <Button appName={"Admin Page"}>Click Me, I am Admin!</Button>
+        </div>
+    )
+}
+
+export default Admin
